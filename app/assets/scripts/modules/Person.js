43e7267dd37d83c4ffe5a6922c204b61d5b12de2
Yegypko <egypko@gmail.com>
2018-09-30 20:48:35 +0300

    js organization end

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,22 @@
+// function Person(fullName, favColor) {
+//   this.name = fullName;
+//   this.favoriteColor = favColor;
+//   this.greet = function() {
+//     console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+//   }
+// }
+
+// module.exports = Person;
+
+class Person {
+  constructor(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+  }
+
+  greet() {
+    console.log("Hi there, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  }
+}
+
+export default Person;
