5c4bf45b4aa3afaace72d8103d1db1d6d34dd0a9
yegypko <egypko@gmail.com>
2018-10-03 18:12:32 +0300

    End header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,26 @@
-var $ = require('jquery');
+// var $ = require('jquery');
 // var Person = require('./modules/Person');
-import Person from './modules/Person';
+// import Person from './modules/Person';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
+// class Adult extends Person {
+//   payTaxes() {
+//     console.log(this.name + " now owes $0 in taxes.");
+//   }
+// }
 
-alert("ABC 321");
+// alert("ABC 321");
 
-var john = new Person("John Doe", "blue");
-john.greet();
+// var john = new Person("John Doe", "blue");
+// john.greet();
 
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+// var jane = new Adult("Jane Smith", "orange");
+// jane.greet();
+// jane.payTaxes();
 
-$("h1").remove();
+// $("h1").remove();
+
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+
+var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
